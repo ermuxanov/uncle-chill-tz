fcae2057f01c96ec357da9984ae95f2c0ed90d6c
Kasym <ermuxanov.98@mail.ru>
2025-03-19 18:04:15 +0500

    add header desctop and mobile

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
 import Swiper from "swiper";
 import { plugins } from "./components/plugins.js";
 import { intro } from "./sections/intro.js";
+import { header } from "./components/header.js";
 
 window.addEventListener("onbeforeunload", function () {
-    window.scrollTo(0, 0);
-    gsap.to(window, { duration: 0, scrollTo: 0 });
+  window.scrollTo(0, 0);
+  gsap.to(window, { duration: 0, scrollTo: 0 });
 });
 window.addEventListener("unload", function () {
-    window.scrollTo(0, 0);
-    gsap.to(window, { duration: 0, scrollTo: 0 });
+  window.scrollTo(0, 0);
+  gsap.to(window, { duration: 0, scrollTo: 0 });
 });
 
-
 window.addEventListener("load", () => {
-    plugins()
-    Swiper.use([Navigation, Autoplay])
-
-    intro()
-})
+  plugins();
+  Swiper.use([Navigation, Autoplay]);
+  header();
+  intro();
+});
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,3 +1,9 @@
 export function header() {
-
+  const text = document.querySelector(".header__menu-text p");
+  const repeat = Number(text.getAttribute("data-repeat"));
+  const repeatedText = (text.innerText.trim() + " ").repeat(repeat);
+  text.innerHTML = repeatedText
+    .split("")
+    .map((char, i, arr) => `<span style="transform:rotate(${(i * 360) / arr.length}deg)">${char}</span>`)
+    .join("");
 }
